99d42f490a290ad56fe924b5b4520aeee11c403b
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-31 19:00:43 -0300

    update

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -5,5 +5,6 @@ var bicicletaController = require("../../controllers/api/bicicletaControllerApi"
 router.get("/", bicicletaController.bicicleta_list);
 router.post("/create", bicicletaController.bicicleta_create);
 router.post("/delete", bicicletaController.bicicleta_delete);
+router.post("/:id/update", bicicletaController.bicicleta_update);
 
 module.exports = router;
