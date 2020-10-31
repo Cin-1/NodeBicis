ebdd84ebf2d76ea8300663e38ea90274ded2eb89
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-31 14:43:22 -0300

    crud

--- Model/bicicleta.js
+++ Model/bicicleta.js
@@ -15,8 +15,23 @@ Bicicleta.add = function (aBici) {
   Bicicleta.allBicis.push(aBici);
 };
 
+Bicicleta.findById = function (aBiciId) {
+  let aBici = Bicicleta.allBicis.find((x) => x.id === aBiciId);
+  if (aBici) return aBici;
+  else throw new Error(`No existe una bici con el id ${aBiciId}`);
+};
+
+Bicicleta.removeById = function (aBiciId) {
+  for (let index = 0; index < Bicicleta.allBicis.length; index++) {
+    if (Bicicleta.allBicis[index].id === aBiciId) {
+      Bicicleta.allBicis.splice(index, 1);
+      break;
+    }
+  }
+};
+
 var a = new Bicicleta(1, "rojo", "deportiva", [-34.571358, -58.633972]);
-var b = new Bicicleta(1, "azul", "playera", [-34.574359, -58.642015]);
+var b = new Bicicleta(2, "azul", "playera", [-34.574359, -58.642015]);
 
 Bicicleta.add(a);
 Bicicleta.add(b);
